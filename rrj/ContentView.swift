4a8086fd86d8aa3a527868ff614a83d30f35cb80
Rishav Raj Jain <rishavrajjain@gmail.com>
2024-06-15 11:38:50 +0530

    Initial Commit

---
+++ rrj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rrj
+//
+//  Created by Rishav Raj Jain on 15/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rrj/rrjApp.swift
@@ -0,0 +1,17 @@
+//
+//  rrjApp.swift
+//  rrj
+//
+//  Created by Rishav Raj Jain on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct rrjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
